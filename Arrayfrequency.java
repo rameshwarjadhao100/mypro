32c34e80c0b084d5eccd08a444a5747147dc2289
rameshwarjadhao100 <rameshawarjadhao100@gmail.com>
2022-04-09 13:31:09 +0530

    myjavapro

---
+++ Arrayfrequency.java
@@ -0,0 +1,39 @@
+package Arratprogram;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
+
+public class Arrayfrequency {
+	
+
+		public static void main(String[] args) {
+			
+		 String [] array = {"a","b","c","a","f","a","c"};
+		 List <String> list = Arrays.asList(array);
+		 TreeSet<String> ts = new TreeSet<String>(list);
+		 System.out.println(ts);
+			
+			
+		
+//			for (String s:ts) {
+////				System.out.println(s +" " + Collections.frequency(list,s));
+//				int count =0;
+//				for (int i=0;i<array.length;i++) {
+//					if (array[i]==s){
+//						count++;
+//					}	
+//						
+//					}
+//					System.out.println(s +"="+count);
+//				
+//				
+//				
+//			}
+				}			
+	}
+//
+//
+//
+//
+
---
+++ duplicatesStreing.java
@@ -0,0 +1,14 @@
+package Arratprogram;
+
+public class duplicatesStreing {
+
+	public static void findDuplicates(String str ) {
+		// TODO Auto-generated method stub
+    String Str ="i am am rameshwar i am student";
+    String [] arr =Str .split(" ");
+    for( String  d:arr ) {
+    	System.out.println();
+    }
+	}
+
+}
